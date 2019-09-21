ea59ee99fcfd4bf3d3a97e3ddead37e9b9494426
DanielBarcenas97 <barcenas.proteco@gmail.com>
2019-09-21 18:46:28 -0500

    Se añade memoria dinamica, se hacen funciones

--- ejer.c
+++ ejer.c
@@ -2,73 +2,44 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+int **memoriaMatriz (int n,int m) {
+	int i;
+	int **mat;
+	
+	mat= (int **) calloc (n,sizeof (int *));
+	for (i= 0; i < n; i++)
+		mat[i]= (int *) calloc (m,sizeof (int));
+	return mat;
+}
 
 
-int main(int argv, char **args){
-    char **nombreArchivo = &args[1];
-	FILE *archivo;
-	char caracter;
-	archivo = fopen(*nombreArchivo,"r");
-    int renglones=0;
-    int columnas=0;
-    char arregloMapa[2];
-    
-    
-    int i=0;
 
-	if (archivo == NULL){
-        printf("\nError al abrir el archivo. \n\n");
+void imprimeMatriz (int **arregloMapa2, int n, int m) {
+	int i,j;
+	printf("Dentro \n");
+	for (i= 0; i < n; i++) {
+		for (j= 0; j < m; j++){
+            if(arregloMapa2[i][j] == '*'){
+                    printf("%c ",arregloMapa2[i][j]);
+                }else{
+                    printf("%d ",arregloMapa2[i][j] );
+                }
+        }
+		printf ("\n");
+	}
+}
+
+void Check(int **arregloMapa2, int i, int j){
+    if(arregloMapa2[i][j+1]  == '*' ){
+        arregloMapa2[i][j+1]  = '*'
+    }else{
+        arregloMapa2[i][j+1] += 1;
     }
-    else{
-        //Leer el primer renglon y guardar valores de renglon y columna 
-         while((caracter = fgetc(archivo)) != '\n'){
-            //printf("%c \n",caracter);
-            if (isdigit(caracter)){
-                arregloMapa[i]=caracter;
-                i+=1;
-            }  
-        }
-        //printf("%s\n\n",arregloMapa);
-        //volvemos a enteros los caracteres en char
-        renglones=arregloMapa[0] - '0';
-        columnas=arregloMapa[1]  - '0';
-        int dimension= renglones * columnas;        
-        printf("Las dimensiones son: [%d]x[%d] \n",renglones,columnas);
-        //printf(" ->%d \n",renglones);
-        //printf(" ->%d \n",columnas);
+}
 
-        int arregloMapa2[renglones][columnas];
-        i=0;
-        int j=0;
-        //printf("\nEl contenido es: \n");
-        while((caracter = fgetc(archivo)) != EOF){
-            printf("%c",caracter);
-            if(caracter != '\n'){
-                arregloMapa2[i][j]=caracter;
-                j+=1;
-                if(j==renglones){
-                    i+=1;
-                    j=0;
-                }
-            }            
-	    }
-        /////Todo bien
-        printf("\nEl contenido es: xd \n");
-        for (i = 0; i < renglones; i++){
-            for (j = 0; j < columnas; j++){
-                if(arregloMapa2[i][j] == '.'){
-                  arregloMapa2[i][j]=0 ;  
-                }
-                if(arregloMapa2[i][j] == '*'){
-                  arregloMapa2[i][j]= '*' ;  
-                }
-                printf("%d ",arregloMapa2[i][j]);
-            }
-            printf("\n");
-        }
-        //Lugar donde estan las bombas
-
-        for (i = 0; i < renglones; i++){
+void bombasActivas(int **arregloMapa2, int renglones, int columnas) {
+	int i,j;
+	for (i = 0; i < renglones; i++){
             for (j = 0; j < columnas; j++){
                 printf("-->%d ",arregloMapa2[i][j]);
             
@@ -144,23 +115,52 @@ int main(int argv, char **args){
             }
             printf("\n");
         }
+}
 
-        printf("\n");
-        printf("\n");
-        printf("\nEl contenido es: \n");
-        for (i = 0; i < renglones; i++){
-            for (j = 0; j < columnas; j++){
-                if(arregloMapa2[i][j] == '*'){
-                    printf("%c ",arregloMapa2[i][j]);
-                }else{
-                    printf("%d ",arregloMapa2[i][j] );
-                }
-                
-            }
-            printf("\n");
+int main(int argv, char **args){
+    char **nombreArchivo = &args[1];
+	FILE *archivo;
+	char caracter;
+	archivo = fopen(*nombreArchivo,"r");
+    char arregloMapa[2];
+    int **arregloMapa2;
+    int renglones=0;
+    int columnas=0;
+    int i=0,j=0;
+	if (archivo == NULL){
+        printf("\nError al abrir el archivo. \n\n");
+    }
+    else{
+         while((caracter = fgetc(archivo)) != '\n'){//Leer el primer renglon y guardar valores de renglon y columna 
+            if (isdigit(caracter)){
+                arregloMapa[i]=caracter;
+                i+=1;
+            }  
         }
-        printf("\n");
-
+        renglones=arregloMapa[0] - '0';//volvemos a enteros los caracteres en char
+        columnas=arregloMapa[1]  - '0';
+        arregloMapa2=memoriaMatriz(renglones,columnas);
+        i=0;
+        while((caracter = fgetc(archivo)) != EOF){
+            printf("%c",caracter);
+            if(caracter != '\n'){
+                arregloMapa2[i][j]=caracter;
+                if(arregloMapa2[i][j] == '.'){
+                    arregloMapa2[i][j]=0 ;  
+                }
+                if(arregloMapa2[i][j] == '*'){
+                    arregloMapa2[i][j]= '*' ;  
+                }
+                j+=1;
+                if(j==renglones){
+                    i+=1;
+                    j=0;
+                }
+            }            
+	    }
+        //Lugar donde estan las bombas
+        bombasActivas(arregloMapa2,renglones,columnas);
+        imprimeMatriz(arregloMapa2,renglones,columnas);
     }
     fclose(archivo);
 	return 0;
